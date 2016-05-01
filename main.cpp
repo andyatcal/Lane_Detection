6d71a9b71b9f924e9c703440d72a4fe0557701ed
andyatcal <andyatcal@berkeley.edu>
2016-05-01 15:08:25 -0700

    video capture added

--- main.cpp
+++ main.cpp
@@ -7,8 +7,32 @@ using namespace std;
 
 //VisualSystem g_v;
 
-int main(int argc, char **argv) {
-    char c;
+int main(int argc, char **argv)
+{
+    //Import a video.
+    VideoCapture *capture;
+
+    if (argc > 1) //From a file
+    {
+        capture = new VideoCapture(argv[1]);
+        if (!capture->isOpened()) {
+            printf("Can't open your file");
+            exit(0);
+        }
+    } 
+    else // From camera
+    {
+        capture = new VideoCapture(0); // From the camera
+    }
+
+    cvNamedWindow("videoImage", CV_WINDOW_AUTOSIZE); 
+    cvMoveWindow("videoImage", 30, 0);
+    Mat img_src;
+    while(1) {
+        *capture >> img_src;
+        imshow("videoImage", img_src);
+        waitKey(1);
+    }
 
     return 0;
 }
---
+++ run.sh
@@ -0,0 +1,3 @@
+g++ main.cpp -o main -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_ml -lopencv_video
+
+./main archive/LaneTest.mp4
