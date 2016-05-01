5a8ad0ad17e60deb9a58a85381047eb66ac43e6e
andyatcal <andyatcal@berkeley.edu>
2016-05-01 14:50:34 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+//VisualSystem g_v;
+
+int main(int argc, char **argv) {
+    char c;
+
+    return 0;
+}
