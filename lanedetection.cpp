2d9f58f8eb12dbe7eec3e5235f6250aff712a8a7
andyatcal <andyatcal@berkeley.edu>
2016-05-02 14:29:11 -0700

    points added

--- lanedetection.cpp
+++ lanedetection.cpp
@@ -24,16 +24,61 @@ void LaneDetection::detect(Mat &input)
     cvtColor(img_src, img_gray, CV_BGR2GRAY);
     blur(img_gray, img_gray, Size(3,3));
     Canny(img_roi, img_roi, CANNY_THRESHOLD_LOW, CANNY_THRESHOLD_HIGHT, 3);
-    HoughLinesP(img_roi, houghlines, 1, CV_PI/180, 20, 10, 3);
-    plotHoughLines(img_src);
+    HoughLinesP(img_roi, houghlines, 1, CV_PI/180, 20, 5, 3);
+    filterHoughLines();
+    renewHoughLinePoints();
+    //plotHoughLines(img_src);
+    plotHoughLinesPoints(img_src);
+    findLaneLines();
 }
 
 void LaneDetection::plotHoughLines(Mat &input)
 {
-    for( size_t i = 0; i < houghlines.size(); i++ )
+    for( size_t i = 0; i < filteredHoughlines.size(); i++ )
     {
         //std::cout<<houghlines[i][0]<<" "<<houghlines[i][1]<<" "<<houghlines[i][2]<<" "<<houghlines[i][3]<<endl;
-        line(input, Point(houghlines[i][0], houghlines[i][1] + roi_start_y),
-            Point(houghlines[i][2], houghlines[i][3] + roi_start_y), Scalar(0,0,255), 3, 8 );
+        line(input, Point(filteredHoughlines[i][0], filteredHoughlines[i][1] + roi_start_y),
+            Point(filteredHoughlines[i][2], filteredHoughlines[i][3] + roi_start_y), Scalar(0,0,255), 2, 20);
     }
+}
+
+void LaneDetection::plotHoughLinesPoints(Mat &input)
+{
+    for( size_t i = 0; i < houghlinespoints.size(); i++ )
+    {
+        circle(input, houghlinespoints[i], 3 , Scalar(0, 0, 255), -1, 8 );
+    }
+}
+
+void LaneDetection::filterHoughLines()
+{
+    float k;
+    filteredHoughlines.clear();
+    for( size_t i = 0; i < houghlines.size(); i++ )
+    {
+        if(houghlines[i][0] == houghlines[i][2])
+        {
+            filteredHoughlines.push_back(houghlines[i]);
+        }
+        k = (1.0 * houghlines[i][3] - 1.0 * houghlines[i][1]) / (1.0 * houghlines[i][2] - 1.0 * houghlines[i][0]);
+        if(abs(k) > 0.2) {
+            filteredHoughlines.push_back(houghlines[i]);
+        }
+    }
+}
+
+void LaneDetection::renewHoughLinePoints()
+{
+    houghlinespoints.clear();
+    for( size_t i = 0; i < filteredHoughlines.size(); i++ )
+    {
+        houghlinespoints.push_back(Point(filteredHoughlines[i][0],filteredHoughlines[i][1] + roi_start_y));
+        houghlinespoints.push_back(Point(filteredHoughlines[i][2],filteredHoughlines[i][3] + roi_start_y));
+    }
+}
+
+void LaneDetection::findLaneLines()
+{
+    Vec4i newLine;
+    //fitLine(houghlinespoints, newLine, CV_DIST_L2, 0, 0.01, 0.01);
 }
--- lanedetection.h
+++ lanedetection.h
@@ -31,9 +31,33 @@ public:
 
     /**
      * Plot the hough lines.
-     * Paint the lines on the input.
      */
     void plotHoughLines(Mat &input);
+
+    /**
+     * Plot the points on the hough lines.
+     */
+    void plotHoughLinesPoints(Mat &input);
+
+    /**
+     * Filter out some HoughLines.
+     */
+    void filterHoughLines();
+
+    /**
+     * Debug function to get the average of all x coordinates. 
+     */
+    void avgOfX();
+
+    /**
+     * Put all points from the Filtered Hough Lines into a vector. 
+     */
+    void renewHoughLinePoints();
+
+    /**
+     * Find the lane seperate lines.
+     */
+    void findLaneLines();
 private:
     /* Store the width and height of the image. */
     int img_width, img_height;
@@ -41,6 +65,12 @@ private:
     int roi_start_y, roi_height;
     /* Store the hough lines of the image. */
     vector<Vec4i> houghlines;
+    /* Filtered hough lines. */
+    vector<Vec4i> filteredHoughlines;
+    /* Store the points of houghlines here. */
+    vector<Point> houghlinespoints;
+    /* Store the lane lines.*/
+    vector<Vec4i> lanelines;
 };
 
 #endif //LANEDETECTION_H_
